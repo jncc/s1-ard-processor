89b57c7d80a819a87a1036ba1c93b117182e68e3
Cathy Jin <cathy.jin@jncc.gov.uk>
2018-08-02 10:22:47 +0000

    Removing subtree

--- app/workflows/process_s1_scene/CheckFileExists.py
+++
@@ -1,8 +0,0 @@
-import luigi
-from luigi import LocalTarget
-
-class CheckFileExists(luigi.ExternalTask):
-    filePath = luigi.Parameter()
-
-    def output(self):
-        return LocalTarget(self.filePath)
