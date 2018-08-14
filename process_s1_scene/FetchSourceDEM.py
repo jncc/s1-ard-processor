fd5d074a632d5c1cf469d5eed891c5980195e67e
Felix Mason <felix.mason@jncc.gov.uk>
2018-08-14 12:22:35 +0100

    Squashed 'workflow/app/workflows/' content from commit c33f73d
    git-subtree-dir: workflow/app/workflows
    git-subtree-split: c33f73d98b0a3ca30c5ec252ccbba07fae4b0d1e

---
+++ process_s1_scene/FetchSourceDEM.py
@@ -0,0 +1,30 @@
+import boto3
+import botocore
+import luigi
+import os
+import logging
+import json
+import process_s1_scene.common as wc
+
+from workflow_common.s3 import getPathFromS3Path, getBucketNameFromS3Path
+from luigi.util import requires
+from luigi import LocalTarget
+from process_s1_scene.InitialiseDataFolder import InitialiseDataFolder
+
+log = logging.getLogger('luigi-interface')
+
+@requires(InitialiseDataFolder)
+class FetchSourceDEM(luigi.Task):
+    sourceFile = luigi.Parameter()
+    pathRoots = luigi.DictParameter()
+    productId = luigi.Parameter()
+    processToS3 = luigi.BoolParameter(default=False)
+
+    def run(self):
+        if self.processToS3:
+            s3 = boto3.resource('s3')
+            s3.Bucket(getBucketNameFromS3Path(self.pathRoots["source-dem-s3-path"])).download_file(getPathFromS3Path(self.pathRoots["source-dem-s3-path"]), self.pathRoots['local-dem-path'])
+
+    def output(self):
+        return LocalTarget(self.pathRoots['local-dem-path'])
+        
