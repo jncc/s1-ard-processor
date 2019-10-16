950249fb803e2dc01fb2a41487d24f3c4e328e52
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2019-10-16 11:53:17 +0100

    Squashed 'workflow/app/workflows/' content from commit bf3be87
    git-subtree-dir: workflow/app/workflows
    git-subtree-split: bf3be8780e17dc490a07917d78f32d0b5f9074a2

---
+++ process_s1_scene/GetConfiguration.py
@@ -0,0 +1,47 @@
+import luigi
+import json
+import os
+import process_s1_scene.common as wc
+
+from luigi import LocalTarget
+from process_s1_scene.CheckFileExists import CheckFileExists
+from process_s1_scene.EnforceZip import EnforceZip
+from luigi.util import requires
+
+@requires(EnforceZip)
+class GetConfiguration(luigi.Task):
+    paths = luigi.DictParameter()
+    #For state copy options in the docker container
+    noStateCopy = luigi.BoolParameter()
+
+    def getOutputPathFromProductId(self, root, productId):
+        year = productId[4:8]
+        month = productId[8:10]
+        day = productId[10:12]
+
+        return os.path.join(os.path.join(os.path.join(os.path.join(root, year), month), day), productId)
+
+    def run(self):
+        enforceZipInfo = {}
+        with self.input().open('r') as enforceZip:
+            enforceZipInfo = json.load(enforceZip)
+
+        inputFilePath = os.path.join(self.paths["input"], enforceZipInfo["zippedFileName"])
+
+        check = CheckFileExists(inputFilePath)
+        yield check
+
+        productId = wc.getProductIdFromSourceFile(inputFilePath)
+        with self.output().open("w") as outFile:
+            outFile.write(json.dumps({
+                "inputFilePath" : inputFilePath,
+                "productPattern" : wc.getProductPatternFromSourceFile(inputFilePath),
+                "productId" : productId,
+                "workingRoot" : os.path.join(self.paths['working'], productId),
+                "noCopyState" : self.noStateCopy,
+                "outputPath" : self.getOutputPathFromProductId(self.paths['output'], productId)
+            }))
+
+    def output(self):
+        outFile = os.path.join(self.paths['state'], 'GetConfiguration.json')
+        return LocalTarget(outFile)
