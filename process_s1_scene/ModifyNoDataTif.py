8335f65ea7eb79469f6d17e337b1256741558d68
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2019-10-22 10:34:03 +0100

    Squashed 'workflow/app/workflows/' changes from edfc955..d0b4742
    d0b4742 Adding gdal commands to ModifyNoDataTif
    
    git-subtree-dir: workflow/app/workflows
    git-subtree-split: d0b4742fe1bbe2f18e8845038508c48167aea128

--- process_s1_scene/ModifyNoDataTif.py
+++ process_s1_scene/ModifyNoDataTif.py
@@ -20,26 +20,45 @@ class ModifyNoDataTif(luigi.Task):
 
     def run(self):
         addMergedOverviewsInfo = {}
-        with self.input()[2].open('r') as addMergedOverviews:
+        with self.input().open('r') as addMergedOverviews:
             addMergedOverviewsInfo = json.load(addMergedOverviews)
 
-        mergedFile = addMergedOverviewsInfo["overviewsAddedTo"]
+        mergedFilePath = addMergedOverviewsInfo["overviewsAddedTo"]
+        mergedFileName = os.path.splitext(os.path.basename(mergedFilePath))[0]
+        outputFileName = os.path.join(self.paths["working"], mergedFileName+"-mode.tif")
+        
         cmdString1 = 'gdal_calc.py --debug -A {} --A_band=1 --outfile=tmp-band1.tif --calc=nan_to_num(A) --NoDataValue=0 --overwrite' \
-            .format(mergedFile) 
+            .format(mergedFilePath)
+        cmdString2 = 'gdal_calc.py --debug -A {} --A_band=2 --outfile=tmp-band2.tif --calc=nan_to_num(A) --NoDataValue=0 --overwrite' \
+            .format(mergedFilePath)
+        cmdString3 = 'gdalbuildvrt -separate temp.vrt tmp-band1.tif tmp-band2.tif'
+        cmdString4 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 temp.vrt {}-mode-pre.tif' \
+            .format(mergedFileName)
+        cmdString5 = 'gdaladdo -r nearest {}-mode-pre.tif 2 4 8 16 32 64 128 256 512' \
+            .format(mergedFileName)
+        cmdString6 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 --config GDAL_TIFF_OVR_BLOCKSIZE 512 -co COPY_SRC_OVERVIEWS=YES {}-mode-pre.tif {}-mode.tif' \
+            .format(mergedFileName, outputFileName)
 
         if not self.testProcessing:
-            log.info('Creating merged product from Gamma VH & VV bands')
+            log.info('Running commands to modify no data values and perform cloud optimising')
             try:
-                subprocess.check_output(cmdString1, shell=True) 
+                subprocess.check_output(cmdString1, shell=True)
+                subprocess.check_output(cmdString2, shell=True)
+                subprocess.check_output(cmdString3, shell=True)
+                subprocess.check_output(cmdString4, shell=True)
+                subprocess.check_output(cmdString5, shell=True)
+                subprocess.check_output(cmdString6, shell=True)
             except subprocess.CalledProcessError as e:
                 errStr = "command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output)
                 log.error(errStr)
                 raise RuntimeError(errStr)
         else:
-            wc.createTestFile(outputFile, "TEST_FILE")
+            wc.createTestFile(outputFileName, "TEST_FILE")
 
-        with self.output().open("w") as outFile:
-            outFile.write(json.dumps(state))
+        with self.output().open('w') as out:
+            out.write(json.dumps({
+                "modifyNoDataTif" : outputFileName
+            }))
                 
     def output(self):
         outputFile = os.path.join(self.paths["state"], 'ModifyNoDataTif.json')
