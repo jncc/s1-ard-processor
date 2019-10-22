1c499d171e21fc53831696325ba18420b6d96e94
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2019-10-22 17:17:02 +0100

    Squashed 'workflow/app/workflows/' changes from d0b4742..738263c
    738263c Fixing commands
    
    git-subtree-dir: workflow/app/workflows
    git-subtree-split: 738263c027e400f64ab7a19818d8ae7770f857b8

--- process_s1_scene/ModifyNoDataTif.py
+++ process_s1_scene/ModifyNoDataTif.py
@@ -9,35 +9,47 @@ import re
 from luigi import LocalTarget
 from luigi.util import requires
 from process_s1_scene.AddMergedOverviews import AddMergedOverviews
+from process_s1_scene.ConfigureProcessing import ConfigureProcessing
 from os.path import join as joinPath
 
 log = logging.getLogger('luigi-interface')
 
-@requires(AddMergedOverviews)
+@requires(AddMergedOverviews, ConfigureProcessing)
 class ModifyNoDataTif(luigi.Task):
     paths = luigi.DictParameter()
     testProcessing = luigi.BoolParameter()
 
     def run(self):
         addMergedOverviewsInfo = {}
-        with self.input().open('r') as addMergedOverviews:
+        with self.input()[0].open('r') as addMergedOverviews:
             addMergedOverviewsInfo = json.load(addMergedOverviews)
 
+        configureProcessingInfo = {}
+        with self.input()[1].open('r') as configureProcessing:
+            configureProcessingInfo = json.load(configureProcessing) 
+
+        tempOutputDir = configureProcessingInfo["parameters"]["s1_ard_temp_output_dir"]
         mergedFilePath = addMergedOverviewsInfo["overviewsAddedTo"]
         mergedFileName = os.path.splitext(os.path.basename(mergedFilePath))[0]
-        outputFileName = os.path.join(self.paths["working"], mergedFileName+"-mode.tif")
+        outputFileName = os.path.join(tempOutputDir, mergedFileName+"-mode.tif")
+
+        tmpBand1Path = os.path.join(tempOutputDir, "tmp-band1.tif")
+        tmpBand2Path = os.path.join(tempOutputDir, "tmp-band2.tif")
+        tempStackedPath = os.path.join(tempOutputDir, "tmp.vrt")
+        tempModePrePath = os.path.join(tempOutputDir, mergedFileName+"-mode-pre.tif")
         
-        cmdString1 = 'gdal_calc.py --debug -A {} --A_band=1 --outfile=tmp-band1.tif --calc=nan_to_num(A) --NoDataValue=0 --overwrite' \
-            .format(mergedFilePath)
-        cmdString2 = 'gdal_calc.py --debug -A {} --A_band=2 --outfile=tmp-band2.tif --calc=nan_to_num(A) --NoDataValue=0 --overwrite' \
-            .format(mergedFilePath)
-        cmdString3 = 'gdalbuildvrt -separate temp.vrt tmp-band1.tif tmp-band2.tif'
-        cmdString4 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 temp.vrt {}-mode-pre.tif' \
-            .format(mergedFileName)
-        cmdString5 = 'gdaladdo -r nearest {}-mode-pre.tif 2 4 8 16 32 64 128 256 512' \
-            .format(mergedFileName)
-        cmdString6 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 --config GDAL_TIFF_OVR_BLOCKSIZE 512 -co COPY_SRC_OVERVIEWS=YES {}-mode-pre.tif {}-mode.tif' \
-            .format(mergedFileName, outputFileName)
+        cmdString1 = 'gdal_calc.py --debug -A {} --A_band=1 --outfile={} --calc="nan_to_num(A)" --NoDataValue=0 --overwrite' \
+            .format(mergedFilePath, tmpBand1Path)
+        cmdString2 = 'gdal_calc.py --debug -A {} --A_band=2 --outfile={} --calc="nan_to_num(A)" --NoDataValue=0 --overwrite' \
+            .format(mergedFilePath, tmpBand2Path)
+        cmdString3 = 'gdalbuildvrt -separate {} {} {}' \
+            .format(tempStackedPath, tmpBand1Path, tmpBand2Path)
+        cmdString4 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 {} {}' \
+            .format(tempStackedPath, tempModePrePath)
+        cmdString5 = 'gdaladdo -r nearest {} 2 4 8 16 32 64 128 256 512' \
+            .format(tempModePrePath)
+        cmdString6 = 'gdal_translate -co COMPRESS=DEFLATE -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 --config GDAL_TIFF_OVR_BLOCKSIZE 512 -co COPY_SRC_OVERVIEWS=YES {} {}' \
+            .format(tempModePrePath, outputFileName)
 
         if not self.testProcessing:
             log.info('Running commands to modify no data values and perform cloud optimising')
