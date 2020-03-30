4543295815cffd092844ba25ee08129a23d56b90
felix mason <felix.mason@jncc.gov.uk>
2020-03-30 13:47:15 +0100

    add sqllite db

--- workflow/app/workflows/process_s1_scene/GenerateReport.py
+++ workflow/app/workflows/process_s1_scene/GenerateReport.py
@@ -1,6 +1,7 @@
 import luigi
 import os
 import csv
+import sqlite3
 import json
 import re
 
@@ -14,6 +15,7 @@ from process_s1_scene.GetConfiguration import GetConfiguration
 class GenerateReport(luigi.Task):
     paths = luigi.DictParameter()
     reportFileName = luigi.Parameter()
+    dbFileName = luigi.OptionalParameter(default=None)
 
     def parseInputName(self, productId):
         pattern = re.compile("S1([AB])\_IW_GRDH_1SDV\_((20[0-9]{2})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}))")
@@ -25,6 +27,43 @@ class GenerateReport(luigi.Task):
         captureTime = "%s:%s:%s" % (m.group(6), m.group(7), m.group(8)) 
 
         return [productId, satellite, captureDate, captureTime]
+
+    def writeToCsv(self, reportLine, reportFilePath):
+        exists = os.path.isfile(reportFilePath)
+
+        if exists:
+            openMode = "a"
+        else:
+            openMode = "w"
+
+        with open(reportFilePath, openMode, newline='') as csvFile: 
+            writer = csv.writer(csvFile)
+
+            if not exists:  
+                writer.writerow(["ProductId", "Platform", "Capture Date", "Capture Time", "ARD ProductId"])
+
+            writer.writerow(reportLine)   
+
+    def writeToDb(self, reportLine, dbPath):
+        conn = sqlite3.connect(dbPath)
+
+        c = conn.cursor()
+        c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='s1ArdProducts' ''')
+
+        if c.fetchone()[0] != 1: 
+            c.execute('''CREATE TABLE s1ArdProducts
+                        (productId text, platform text, captureDate text, CaptureTime text, ardProductId text)''')
+            
+            conn.commit()
+
+        sql = "INSERT INTO s1ArdProducts VALUES (?,?,?,?,?)"
+
+        row = (reportLine[0], reportLine[1], reportLine[2], reportLine[3], reportLine[4])
+
+        c.execute(sql, row)
+
+        conn.commit()
+        conn.close()
 
     def run(self):
         transferFinalOutputInfo = {}
@@ -43,20 +82,12 @@ class GenerateReport(luigi.Task):
 
         reportFilePath = os.path.join(self.paths["report"], self.reportFileName)
 
-        exists = os.path.isfile(reportFilePath)
+        self.writeToCsv(reportLine, reportFilePath)
 
-        if exists:
-            openMode = "a"
-        else:
-            openMode = "w"
+        if self.dbFileName:
+            dbFilePath = os.path.join(self.paths["report"], self.dbFileName)
 
-        with open(reportFilePath, openMode, newline='') as csvFile: 
-            writer = csv.writer(csvFile)
-
-            if not exists:  
-                writer.writerow(["ProductId", "Platform", "Capture Date", "Capture Time", "ARD ProductId"])
-
-            writer.writerow(reportLine)            
+            self.writeToDb(reportLine, dbFilePath)
 
         with self.output().open("w") as outFile:
             outFile.write(json.dumps({
