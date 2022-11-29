3bfa62a8a101740fc4aa2d2cf4bc59a395da48e6
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2022-11-29 12:26:20 +0000

    Getting RFI values

--- workflow/app/workflows/process_s1_scene/GetRFIInfo.py
+++ workflow/app/workflows/process_s1_scene/GetRFIInfo.py
@@ -14,18 +14,42 @@ log = logging.getLogger('luigi-interface')
 class GetRFIInfo(luigi.Task):
     paths = luigi.DictParameter()
 
+    def getRFIMitigated(self, inputZip, rfiFile):
+        contents = inputZip.read(rfiFile).decode("utf-8")
+        pattern = "<rfiMitigationApplied>([a-zA-Z]+)<\/rfiMitigationApplied>"
+        rfiMitigated = re.search(pattern, contents).group(1)
+
+        return rfiMitigated
+
+    def getRFIDetected(self, inputZip, rfiFile):
+        contents = inputZip.read(rfiFile).decode("utf-8")
+        pattern = "<rfiDetected>([a-zA-Z]+)<\/rfiDetected>"
+        rfiDetectedBursts = re.findall(pattern, contents)
+
+        rfiDetected = False
+
+        for detected in rfiDetectedBursts:
+            log.info(f'RFI detected: {detected}')
+            
+            if detected.lower() == 'true':
+                rfiDetected = True
+                break
+
+        return rfiDetected
+
     def run(self):
         configuration = {}
         with self.input().open('r') as getConfiguration:
             configuration = json.load(getConfiguration)
 
-        inputZip = configuration["inputFilePath"]
-
-        filesInZip = zipfile.ZipFile(inputZip, 'r').namelist()
+        inputZipPath = configuration["inputFilePath"]
+        inputZip = zipfile.ZipFile(inputZipPath, 'r')
+        
+        filesInZip = inputZip.namelist()
 
         # match the VV and VH RFI filenames
         # e.g. S1A_IW_GRDH_1SDV_20220826T175933_20220826T175958_044727_055726_B7DC.SAFE/annotation/rfi/rfi-s1a-iw-grd-vh-20220826t175933-20220826t175958-044727-055726-002.xml
-        pattern = '(\/annotation\/rfi\/rfi-s1a-iw-grd-[vv][vh]-[a-zA-Z0-9-]*.xml)' 
+        pattern = '(S1[AB]_IW_GRDH_1SDV_[a-zA-Z0-9_]*\.SAFE\/annotation\/rfi\/rfi-s1a-iw-grd-[vv][vh]-[a-zA-Z0-9-]*.xml)' 
 
         matches = []
         for file in filesInZip:
@@ -36,11 +60,11 @@ class GetRFIInfo(luigi.Task):
         vvFile = ''
         vhFile = ''
         if not matches:
-            log.warn('No RFI metadata found in ESA data, this should be present in newer ESA data only')
+            log.info('No RFI metadata found in ESA data')
         else:
             rfiFileCount = len(matches)
             if rfiFileCount is not 2:
-                raise Exception(f'Found {rfiFileCount} RFI metadata files in {inputZip}, expected 2')
+                raise Exception(f'Found {rfiFileCount} RFI metadata files in {inputZipPath}, expected 2')
 
             if 'vv' in matches[0]:
                 vvFile = matches[0]
@@ -54,11 +78,12 @@ class GetRFIInfo(luigi.Task):
         rfiMitigationAppliedVV = 'N/A'
         rfiMitigationAppliedVH = 'N/A'
 
-        log.info(f'vvFile: {vvFile}')
-        log.info(f'vhFile: {vhFile}')
-
         if vvFile and vhFile:
-            # todo extract the files and get the values
+            rfiDetectedVV = self.getRFIDetected(inputZip, vvFile)
+            rfiDetectedVH = self.getRFIDetected(inputZip, vhFile)
+            rfiMitigationAppliedVV = self.getRFIMitigated(inputZip, vvFile)
+            rfiMitigationAppliedVH = self.getRFIMitigated(inputZip, vhFile)
+
   
         with self.output().open('w') as out:
             out.write(json.dumps({
